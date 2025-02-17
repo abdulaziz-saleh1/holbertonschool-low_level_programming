46dd6c67321f6f4e756bc8a41965e90de5384de9
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-02-17 16:55:43 +0000

    alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,10 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet.
  */
-
 void print_alphabet_x10(void)
 {
 int i;
- for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
