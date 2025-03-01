526b60e09f90b9f1a29f219df19947e99c3e1c56
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-01 17:59:56 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,19 +7,19 @@
  */
 int _atoi(char *s)
 {
-int i = 0, sign = 1, num = 0, found_digit = 0;
+int i = 0, sign = 1;
+unsigned int num = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-num = num * 10 + (s[i] - '0');
-found_digit = 1;
-}
-else if (found_digit)
+num = (num * 10) + (s[i] - '0');
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
+}
 i++;
 }
-return (num *sign);
+return (sign * num);
 }
