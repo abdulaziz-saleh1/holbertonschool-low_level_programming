23d34c5c84f64db9340243ee1564d181639af09b
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-01 17:16:44 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: Pointer to the string
+ */
+void _puts(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
