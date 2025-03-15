c6b00b7ba050e456f300ccd35f4f34858c470b1b
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-15 16:43:49 +0000

    tasks2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _calloc - Allocates memory for an array using malloc and initializes it to 0
@@ -11,12 +10,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
+char *ptr;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
-memset(ptr, 0, nmemb * size);
+for (i = 0; i < (nmemb * size); i++)
+ptr[i] = 0;
 return (ptr);
 }
