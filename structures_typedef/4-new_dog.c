01f9625a25c7a3df8b00ba76a7dfb529825266c3
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-15 17:19:53 +0000

    tasks3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,47 +1,35 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - Creates a new dog_t instance and copies name and owner.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- *
- * Return: Pointer to the new dog, or NULL if allocation fails.
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
-char *name_copy, *owner_copy;
-
-if (name == NULL || owner == NULL)
-return (NULL);
-
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
-
-name_copy = malloc(strlen(name) + 1);
-if (name_copy == NULL)
-{
-free(dog);
-return (NULL);
-}
-strcpy(name_copy, name);
-
-owner_copy = malloc(strlen(owner) + 1);
-if (owner_copy == NULL)
-{
-free(name_copy);
-free(dog);
-return (NULL);
-}
-strcpy(owner_copy, owner);
-
-dog->name = name_copy;
-dog->age = age;
-dog->owner = owner_copy;
-
-return (dog);
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	/* Cpunting name pointer*/
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
