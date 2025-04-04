b21abd0b51a27d833a6f2e3eb2126e4bf113675b
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-04 16:23:50 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,17 +5,31 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * main - copies the content of a file to another file.
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success, or exits with appropriate code on failure
- */
+char *create_buffer(char *file)
+{
+char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+exit(99);
+}
+return (buffer);
+}
+
+void close_file(int fd)
+{
+int c = close(fd);
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+
 int main(int ac, char **av)
 {
-int fd_from, fd_to;
-ssize_t r, w;
-char buffer[BUFFER_SIZE];
+int fd_from, fd_to, r, w;
+char *buffer;
 
 if (ac != 3)
 {
@@ -23,10 +37,12 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
+buffer = create_buffer(av[2]);
 fd_from = open(av[1], O_RDONLY);
 if (fd_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+free(buffer);
 exit(98);
 }
 
@@ -34,7 +50,8 @@ fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-close(fd_from);
+free(buffer);
+close_file(fd_from);
 exit(99);
 }
 
@@ -44,8 +61,9 @@ w = write(fd_to, buffer, r);
 if (w != r)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-close(fd_from);
-close(fd_to);
+free(buffer);
+close_file(fd_from);
+close_file(fd_to);
 exit(99);
 }
 }
@@ -53,22 +71,14 @@ exit(99);
 if (r == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-close(fd_from);
-close(fd_to);
+free(buffer);
+close_file(fd_from);
+close_file(fd_to);
 exit(98);
 }
 
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
-
+free(buffer);
+close_file(fd_from);
+close_file(fd_to);
 return (0);
 }
