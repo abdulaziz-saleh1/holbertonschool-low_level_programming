375ca8bcb0c3a398549030b522cbaebecffed7f8
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-04 16:17:17 +0000

    append text to file

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
-#define MAIN_H
+0;136;0c#define MAIN_H
 
 #include <stddef.h>
 #include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
