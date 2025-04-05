6b80cf078b4f3ee7b1a2cc7de4510e447c7345d5
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-05 10:26:20 +0000

    cp 10000

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,10 +8,7 @@
  */
 void print_error(int code, const char *msg, const char *arg)
 {
-	if (arg)
-		dprintf(STDERR_FILENO, msg, arg);
-	else
-		dprintf(STDERR_FILENO, "%s", msg);
+	dprintf(STDERR_FILENO, msg, arg);
 	dprintf(STDERR_FILENO, "\n");
 	exit(code);
 }
@@ -30,35 +27,41 @@ void close_fd(int fd)
 }
 
 /**
- * copy_file_content - Copies content from one file to another.
+ * open_files - Opens source and destination files.
+ * @file_from: Source file path.
+ * @file_to: Destination file path.
+ * @fd_from: Pointer to source file descriptor.
+ * @fd_to: Pointer to destination file descriptor.
+ */
+void open_files(const char *file_from, const char *file_to,
+		 int *fd_from, int *fd_to)
+{
+	*fd_from = open(file_from, O_RDONLY);
+	if (*fd_from == -1)
+		print_error(98, "Error: Can't read from file %s", file_from);
+
+	*fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (*fd_to == -1)
+	{
+		close_fd(*fd_from);
+		print_error(99, "Error: Can't write to %s", file_to);
+	}
+}
+
+/**
+ * transfer_data - Reads from source and writes to destination.
+ * @fd_from: Source file descriptor.
+ * @fd_to: Destination file descriptor.
  * @file_from: Source file path.
  * @file_to: Destination file path.
  */
-void copy_file_content(const char *file_from, const char *file_to)
+void transfer_data(int fd_from, int fd_to,
+		     const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file %s", file_from);
-
-	bytes_read = read(fd_from, buffer, 1024);
-	if (bytes_read == -1)
-	{
-		close_fd(fd_from);
-		print_error(98, "Error: Can't read from file %s", file_from);
-	}
-
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close_fd(fd_from);
-		print_error(99, "Error: Can't write to %s", file_to);
-	}
-
-	while (bytes_read > 0)
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
@@ -67,15 +70,27 @@ void copy_file_content(const char *file_from, const char *file_to)
 			close_fd(fd_to);
 			print_error(99, "Error: Can't write to %s", file_to);
 		}
-		bytes_read = read(fd_from, buffer, 1024);
-		if (bytes_read == -1)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			print_error(98, "Error: Can't read from file %s", file_from);
-		}
 	}
 
+	if (bytes_read == -1)
+	{
+		close_fd(fd_from);
+		close_fd(fd_to);
+		print_error(98, "Error: Can't read from file %s", file_from);
+	}
+}
+
+/**
+ * copy_file_content - Copies content from one file to another.
+ * @file_from: Source file path.
+ * @file_to: Destination file path.
+ */
+void copy_file_content(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+
+	open_files(file_from, file_to, &fd_from, &fd_to);
+	transfer_data(fd_from, fd_to, file_from, file_to);
 	close_fd(fd_from);
 	close_fd(fd_to);
 }
@@ -84,13 +99,12 @@ void copy_file_content(const char *file_from, const char *file_to)
  * main - Entry point. Handles arguments.
  * @argc: Argument count.
  * @argv: Argument vector.
- *
  * Return: 0 on success, error otherwise.
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
-		print_error(97, "Usage: cp file_from file_to", NULL);
+		print_error(97, "Usage: cp file_from file_to", "");
 
 	copy_file_content(argv[1], argv[2]);
 	return (0);
