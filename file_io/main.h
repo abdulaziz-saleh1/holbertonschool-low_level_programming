67b8f4e8bbfff9d6ab91a937847d426576c7bca6
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-05 09:46:44 +0000

    cp

--- file_io/main.h
+++ file_io/main.h
@@ -13,9 +13,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 void print_error(int code, const char *msg, const char *arg);
 void close_fd(int fd);
-void copy_file(const char *file_from, const char *file_to);
-int _putchar(char c);
-#endif
+
+#endif 
